0fdbe04716955b3f7887c17c1b40213cee3d5810
yceugenelai <eugene@bigobject.io>
2016-07-07 15:55:43 +0800

    Update collectData.py

--- collectData.py
+++ collectData.py
@@ -40,9 +40,9 @@ if __name__ == "__main__":
                 if count%60 == 0:
                         count=0
                         #sense.show_message("*",scroll_speed=0.05)
-                        tmp_res = json.loads(getData("http://localhost:9090/cmd$
+                        tmp_res = json.loads(getData("http://localhost:9090/cmd,"select count(*) from collect"))
                         now_count = str(tmp_res['Content']['content'])
-                        sense.show_message( now_count[2:-2] + " rows", scroll_s$
+                        sense.show_message( now_count[2:-2] + " rows", scroll_speed=0.05)
                         sense.load_image("logo8.png")
 
                 sense.set_pixel(7, 6, 20*(count%10)+50, 110, 20*(count%10)+50)
@@ -50,9 +50,6 @@ if __name__ == "__main__":
                 sense.set_pixel(6, 6, 20*(count%10)+50, 110, 20*(count%10)+50)
                 sense.set_pixel(6, 7, 20*(count%10)+50, 110, 20*(count%10)+50)
 
-
-
-                now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
                 humidity = sense.get_humidity()
                 temp = sense.get_temperature()
                 pressure = sense.get_pressure()
@@ -65,10 +62,11 @@ if __name__ == "__main__":
                 acc_x = acceleration['x']
                 acc_y = acceleration['y']
                 acc_z = acceleration['z']
-
+                
+                now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
                 stmt = "insert into collect values('" + now + "','"
-                stmt = stmt + str(humidity) + "','"  + str(temp) + "','" + str($
-                stmt = stmt + "','" + str(roll) + "','" + str(yaw) + "','" + st$
+                stmt = stmt + str(humidity) + "','"  + str(temp) + "','" + str(pressure) + "','" + str(pitch)
+                stmt = stmt + "','" + str(roll) + "','" + str(yaw) + "','" + str(acc_x)
                 stmt = stmt + "','" + str(acc_y) + "','" + str(acc_z) + "')"
 
                 #print now, humidity, temp, pressure, pitch, roll, yaw, acc_x, $
